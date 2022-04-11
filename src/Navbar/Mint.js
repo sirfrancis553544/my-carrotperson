cff6c6616fdfac6ad5091aefc6c11f56f6807a7a
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2022-04-11 17:53:22 +0800

    black

--- src/Navbar/Mint.js
+++ src/Navbar/Mint.js
@@ -7,9 +7,7 @@ export default function ContainedButtons() {
     <Stack direction="row" spacing={2}>
       <Button
         variant="contained"
-        target="_blank"
         href="https://0x3b9ca0f67260adadc4e0d4650c52fe4630b11a7a.netlify.app/"
-        rel="noreferrer"
       >
         Free Mint
       </Button>
