af25372e4ed5f6a96dd9d336ae8180e0a1242384
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2022-04-12 19:13:05 +0800

    pages

--- src/Pages/Mint.js
+++ src/Pages/Mint.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { connect } from "./redux/blockchain/blockchainActions";
-import { fetchData } from "./redux/data/dataActions";
-import * as s from "./styles/globalStyles";
+import { connect } from "../redux/blockchain/blockchainActions";
+import { fetchData } from "../redux/data/dataActions";
+import * as s from "../styles/globalStyles";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
